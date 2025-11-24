2f5ed2af2e7b8a49871c2dba48b4c5b9ba6f92a4
Alice Pellegrini <dev@alicepellegrini.me>
2025-11-24 13:27:39 +0000

    bug fix to 90972e2bce

--- infer/src/bufferoverrun/bufferOverrunModels.ml
+++ infer/src/bufferoverrun/bufferOverrunModels.ml
@@ -1639,7 +1639,7 @@ module JavaString = struct
 
 
   let split_with_limit exp limit_exp =
-    match exp with
+    match limit_exp with
     (* TODO: actually a value of -1 would give a different behavior then 0,
       as the resulting length can be gt exp.length *)
     | Exp.Const (Const.Cint climit) when IntLit.leq climit IntLit.zero ->
